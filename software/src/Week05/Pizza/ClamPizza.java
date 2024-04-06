768387e6b5193b5f10a93e0b4fb6f0d198f22911
juhuigit <122880689+juhuigit@users.noreply.github.com>
2024-04-06 14:13:44 +0900

    DesignPattern04_Factory(ver01)

---
+++ software/src/Week05/Pizza/ClamPizza.java
@@ -0,0 +1,11 @@
+package Week05.Pizza;
+
+public class ClamPizza extends Pizza {
+    public ClamPizza() {
+        name = "Clam Pizza";
+        dough = "Thin crust";
+        sauce = "White garlic sauce";
+        toppings.add("Clams");
+        toppings.add("Grated parmesan cheese");
+    }
+}
---
+++ software/src/Week05/Pizza/PizzaStore.java
@@ -0,0 +1,22 @@
+package Week05.Pizza;
+
+public class PizzaStore {
+    SimplePizzaFactory factory;
+
+    public PizzaStore(SimplePizzaFactory factory) {
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        pizza = factory.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+}
